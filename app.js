19f3a1c94ff0a92fd94d5a2c4ebd1ebda0c25034
Kang-IJ <rkddl6803@naver.com>
2022-10-30 15:47:41 +0900

    feat: add title and comments

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const ajax = new XMLHttpRequest();
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 
-ajax.open("GET", "https://api.hnpwa.com/v0/news/1.json", false);
+ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
-console.log(ajax.response);
+const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+
+  a.href = "#";
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
+  ul.appendChild(li);
+}
+
+document.getElementById("root").appendChild(ul);
